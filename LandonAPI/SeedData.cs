bb96a91827a424cb50f42f1beb91e2e2c52dca6b
TuanAnh <anh.nt.1196@gmail.com>
2019-05-11 11:21:53 +0700

    ok

---
+++ LandonAPI/SeedData.cs
@@ -0,0 +1,42 @@
+﻿using LandonAPI.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandonAPI
+{
+    public static  class SeedData
+    {
+
+        public static async Task InitializeAsync(IServiceProvider provider)
+        {
+            await AddTestData(provider.GetRequiredService<HotelApiDbContext>());
+        }
+
+        public static async Task AddTestData(HotelApiDbContext context)
+        {
+            if(context.Rooms.Any())
+            {
+                return;
+            }
+            context.Rooms.Add(new RoomEntity()
+            {
+                Id = new Guid(),
+                Name = "Oxford Suite",
+                Rate = 1101
+
+            });
+
+            context.Rooms.Add(new RoomEntity()
+            {
+                Id = new Guid(),
+                Name = "Driscoll suite",
+                Rate = 2345
+            });
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
